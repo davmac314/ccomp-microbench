a10732ea8d85d45d397ee4d75fcccdeb62eb243c
Lorin Kazaz <lorin.kazaz@itm.uni-stuttgart.de>
2018-03-20 17:38:13 +0100

    more iterations on bm2

--- bm2/driver.c
+++ bm2/driver.c
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
     struct goober g;
-    for (int i = 0; i < 10000000; i++) {
+    for (int i = 0; i < 10000000*10; i++) {
         testFunc(&g);
     }
     return 0;
